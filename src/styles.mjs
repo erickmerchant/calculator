3602d7812ae4191dc241b3dd95c2c89b39fb40dd
Erick Merchant <erickmerchant@gmail.com>
2020-01-06 20:43:34 -0500

    Updates

--- src/styles.mjs
+++ src/styles.mjs
@@ -1,16 +1,33 @@
 const element = `
-  border-radius: 3px;
-  border: 3px solid var(--background);
-  grid-area: var(--grid-area);
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border-bottom-right-radius: 3px;
+  border-bottom-left-radius: 3px;
+  border-top-width: 3px;
+  border-bottom-width: 3px;
+  border-right-width: 3px;
+  border-left-width: 3px;
+  border-top-style: solid;
+  border-bottom-style: solid;
+  border-right-style: solid;
+  border-left-style: solid;
+  border-top-color: var(--background-color);
+  border-bottom-color: var(--background-color);
+  border-right-color: var(--background-color);
+  border-left-color: var(--background-color);
+  grid-row-start: var(--grid-area);
+  grid-row-end: var(--grid-area);
+  grid-column-start: var(--grid-area);
+  grid-column-end: var(--grid-area);
 
-  --background: hsl(var(--hue), var(--saturation), 50%);
+  --background-color: hsl(var(--hue), var(--saturation), 50%);
 `
 
 const button = `
   ${element}
   color: white;
   min-height: 3em;
-  background: var(--background);
+  background-color: var(--background-color);
 `
 
 export default {
@@ -30,8 +47,14 @@ export default {
     * {
       box-sizing: border-box;
       font: inherit;
-      margin: 0;
-      padding: 0;
+      margin-top: 0;
+      margin-bottom: 0;
+      margin-right: 0;
+      margin-left: 0;
+      padding-top: 0;
+      padding-bottom: 0;
+      padding-right: 0;
+      padding-left: 0;
       max-width: 100%;
     }
 
@@ -50,9 +73,15 @@ export default {
   `,
   form: `
     display: grid;
-    margin: auto;
+    margin-top: auto;
+    margin-bottom: auto;
+    margin-right: auto;
+    margin-left: auto;
     width: 20em;
-    padding: 1em;
+    padding-top: 1em;
+    padding-bottom: 1em;
+    padding-right: 1em;
+    padding-left: 1em;
     gap: 0.25em;
     grid-template-columns: repeat(4, 1fr);
     grid-template-areas:
@@ -70,12 +99,18 @@ export default {
   output: `
     ${element}
     display: flex;
-    padding: 1em;
-    grid-area: output;
+    padding-top: 1em;
+    padding-bottom: 1em;
+    padding-right: 1em;
+    padding-left: 1em;
+    grid-row-start: output;
+    grid-row-end: output;
+    grid-column-start: output;
+    grid-column-end: output;
     justify-content: flex-end;
     line-height: 1;
     height: 3em;
-    color: var(--background);
+    color: var(--background-color);
   `,
   operator: `
     ${button}
