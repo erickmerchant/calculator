3fa609753b61e3c5a84439c23c03803c4ab69dfa
Erick Merchant <erickmerchant@gmail.com>
2020-02-09 00:05:33 -0500

    Update and refactor

--- src/app.mjs
+++ src/app.mjs
@@ -5,122 +5,149 @@ const target = document.querySelector('body')
 
 const update = domUpdate(target)
 
-const button = (commit, onclick, area, txt) => {
-  let className = classes.button
-
-  if (['plus', 'minus', 'times', 'divide', 'equals'].includes(area)) {
-    className = classes.operator
-  }
-
-  if (area === 'clear') {
-    className = classes.clear
-  }
-
-  return html`<button
+const button = (commit, options) => html`<button
     type="button"
-    class=${className}
-    style=${`--grid-area: ${area}`}
+    class=${options.classes || classes.button}
+    style=${`--grid-area: ${options.area}`}
     onclick=${() => {
-      commit(onclick)
-    }}>${txt}</button>`
+      commit(options.onclick)
+    }}>${options.text}</button>`
+
+const defaultState = {
+  output: null,
+  left: null,
+  operator: null,
+  right: null,
+  done: false
 }
 
-const clear = () => {
-  return {
-    output: null,
-    left: null,
-    operator: null,
-    right: null,
-    done: false
+const state = {...defaultState}
+
+const clear = (commit) => button(
+  commit,
+  {
+    onclick() {
+      return {...defaultState}
+    },
+    classes: classes.clear,
+    area: 'clear',
+    text: 'AC'
   }
-}
+)
 
-const state = clear()
+const equals = (commit) => button(
+  commit,
+  {
+    onclick(state) {
+      if (state.left == null || state.operator == null || state.right == null) {
+        return state
+      }
 
-const equals = (state) => {
-  if (state.left == null || state.operator == null || state.right == null) {
-    return state
+      state.output = 'left'
+
+      const left = Number(state.left)
+      const right = Number(state.right)
+
+      switch (state.operator) {
+        case '+':
+          state.left = left + right
+          break
+
+        case '-':
+          state.left = left - right
+          break
+
+        case '*':
+          state.left = left * right
+          break
+
+        case '/':
+          state.left = left / right
+          break
+      }
+
+      state.done = true
+
+      return state
+    },
+    area: 'equals',
+    text: raw('&equals;')
   }
+)
 
-  state.output = 'left'
+const operator = (commit, operator, area, text) => button(
+  commit,
+  {
+    onclick(state) {
+      if (state.left == null) {
+        return state
+      }
 
-  const left = Number(state.left)
-  const right = Number(state.right)
+      if (state.output === 'right') {
+        equals(state)
+      }
 
-  switch (state.operator) {
-    case '+':
-      state.left = left + right
-      break
+      state.right = null
 
-    case '-':
-      state.left = left - right
-      break
+      state.operator = operator
 
-    case '*':
-      state.left = left * right
-      break
+      state.done = false
 
-    case '/':
-      state.left = left / right
-      break
+      return state
+    },
+    classes: classes.operator,
+    area,
+    text
   }
+)
 
-  state.done = true
+const character = (commit, character, area, text) => button(
+  commit,
+  {
+    onclick(state) {
+      if (state.done) {
+        state = clear()
+      }
 
-  return state
-}
+      let target = 'right'
 
-const operator = (operator) => (state) => {
-  if (state.left == null) {
-    return state
+      if (state.operator == null) {
+        target = 'left'
+      }
+
+      if (state[target] == null) {
+        state[target] = character === '.' ? '0.' : character
+      } else if (character !== '.' || !state[target].includes('.')) {
+        state[target] += character
+      }
+
+      state.output = target
+
+      return state
+    },
+    area,
+    text
   }
+)
 
-  if (state.output === 'right') {
-    equals(state)
+const sign = (commit) => button(
+  commit,
+  {
+    onclick(state) {
+      const target = state.output
+
+      if (state[target] != null) {
+        const number = Number(state[target])
+
+        state[target] = number * -1
+      }
+
+      return state
+    },
+    area: 'sign',
+    text: raw('&plusmn;')
   }
-
-  state.right = null
-
-  state.operator = operator
-
-  state.done = false
-
-  return state
-}
-
-const character = (character) => (state) => {
-  if (state.done) {
-    state = clear()
-  }
-
-  let target = 'right'
-
-  if (state.operator == null) {
-    target = 'left'
-  }
-
-  if (state[target] == null) {
-    state[target] = character === '.' ? '0.' : character
-  } else if (character !== '.' || !state[target].includes('.')) {
-    state[target] += character
-  }
-
-  state.output = target
-
-  return state
-}
-
-const sign = (state) => {
-  const target = state.output
-
-  if (state[target] != null) {
-    const number = Number(state[target])
-
-    state[target] = number * -1
-  }
-
-  return state
-}
+)
 
 const format = (val) => {
   if (typeof val === 'number') return val
@@ -149,24 +176,24 @@ render({
     <body class=${classes.app}>
       <form class=${classes.form}>
         <output class=${classes.output}>${state.output ? format(state[state.output]) : '0'}</output>
-        ${button(commit, clear, 'clear', 'AC')}
-        ${button(commit, operator('+'), 'plus', raw('&plus;'))}
-        ${button(commit, operator('-'), 'minus', raw('&minus;'))}
-        ${button(commit, operator('*'), 'times', raw('&times;'))}
-        ${button(commit, character('7'), 'seven', 7)}
-        ${button(commit, character('8'), 'eight', 8)}
-        ${button(commit, character('9'), 'nine', 9)}
-        ${button(commit, operator('/'), 'divide', raw('&divide;'))}
-        ${button(commit, character('4'), 'four', 4)}
-        ${button(commit, character('5'), 'five', 5)}
-        ${button(commit, character('6'), 'six', 6)}
-        ${button(commit, equals, 'equals', raw('&equals;'))}
-        ${button(commit, character('1'), 'one', 1)}
-        ${button(commit, character('2'), 'two', 2)}
-        ${button(commit, character('3'), 'three', 3)}
-        ${button(commit, character('0'), 'zero', 0)}
-        ${button(commit, character('.'), 'decimal', '.')}
-        ${button(commit, sign, 'sign', raw('&plusmn;'))}
+        ${clear(commit)}
+        ${operator(commit, '+', 'plus', raw('&plus;'))}
+        ${operator(commit, '-', 'minus', raw('&minus;'))}
+        ${operator(commit, '*', 'times', raw('&times;'))}
+        ${character(commit, '7', 'seven', 7)}
+        ${character(commit, '8', 'eight', 8)}
+        ${character(commit, '9', 'nine', 9)}
+        ${operator(commit, '/', 'divide', raw('&divide;'))}
+        ${character(commit, '4', 'four', 4)}
+        ${character(commit, '5', 'five', 5)}
+        ${character(commit, '6', 'six', 6)}
+        ${equals(commit)}
+        ${character(commit, '1', 'one', 1)}
+        ${character(commit, '2', 'two', 2)}
+        ${character(commit, '3', 'three', 3)}
+        ${character(commit, '0', 'zero', 0)}
+        ${character(commit, '.', 'decimal', '.')}
+        ${sign(commit)}
       </form>
     </body>`
   }
